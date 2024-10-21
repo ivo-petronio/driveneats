2ed75b72e2d0af270ccd4b8a858055edd3ba2ddf
Cyber_Petronio <kawaiivo@gmail.com>
2024-10-21 08:18:31 -0300

    html stage complete

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/*** JAVASCRIPT ***/
+
+const orderBtn = document.querySelector(".orderBtn .incomplete");
+
+function selectFood(food) {
+    let selectedFood = document.querySelector(`.{food}`);
+    let selectedBefore = document.querySelector(".food .selected");
+}
