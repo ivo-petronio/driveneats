8a930ca0c9da44029c4e80a21e1732546e1c849f
Cyber_Petronio <kawaiivo@gmail.com>
2024-10-21 21:10:23 -0300

    selecting buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,52 @@
-/*** JAVASCRIPT ***/
+/*** JAVASCRIPT ***
+******************/
 
-const orderBtn = document.querySelector(".orderBtn .incomplete");
+
+/*****************
+ Global Variables
+*****************/
+
+var orderItems = new Object();
+
+
+
+/*****************
+    Functions
+*****************/
+
 
 function selectFood(food) {
-    let selectedFood = document.querySelector(`.{food}`);
-    let selectedBefore = document.querySelector(".food .selected");
+	justSelected(document.querySelector('.food .selected'));
+	food.classList.add('selected');
+	console.log(food.children);
+	orderItems.food = {
+				rango: food.children[1].innerHTML,
+				preco: food.children[4].innerHTML
+			  }
+}
+
+function selectDrink(drink) {
+	justSelected(document.querySelector('.drink .selected'));
+	drink.classList.add('selected');
+	const temp = document.querySelector('.drink .selected');
+	console.log(temp.children[1].innerHTML);
+}
+
+function selectDessert(dessert) {
+	justSelected(document.querySelector('.dessert .selected'));
+	dessert.classList.add('selected');
+}
+
+
+
+
+/**************************************************************
+ This function tests if an option has not been selected before
+**************************************************************/
+
+
+function justSelected(selected) {
+	if(selected && selected.className.indexOf('.selected')) {
+		selected.classList.remove('selected');
+	}
 }
