3d65d6bcb3889316df602009c97d6ecb9be124df
Cyber_Petronio <kawaiivo@gmail.com>
2024-10-23 03:24:22 -0300

    order checkout

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ function selectFood(food) {
 	food.classList.add('selected');
 	orderItems.food = {
 				rango: food.children[1].innerHTML,
-				preco: food.children[4].innerHTML
+				price: food.children[4].innerHTML
 			  }
 }
 
@@ -28,19 +28,18 @@ function selectDrink(drink) {
 	justSelected(document.querySelector('.drink .selected'));
 	drink.classList.add('selected');
 	orderItems.drink = {
-				 drink: drink.children[1].innerHTML,
-				 preco: drink.children[4].innerHTML
-			  }
+				drink: drink.children[1].innerHTML,
+				price: drink.children[4].innerHTML
+			   }
 }
 
 function selectDessert(dessert) {
 	justSelected(document.querySelector('.dessert .selected'));
 	dessert.classList.add('selected');
 	orderItems.dessert = {
-				 dessert: dessert.children[1].innerHTML,
-				 preco: dessert.children[4].innerHTML
-	}
-	console.log(orderItems);
+				dessert: dessert.children[1].innerHTML,
+				price: dessert.children[4].innerHTML
+			     }
 }
 
 
@@ -56,3 +55,47 @@ function justSelected(selected) {
 		selected.classList.remove('selected');
 	}
 }
+
+
+
+/******************************************************************
+ This function verifies if all the options have just been selected
+                and ability the order buttom
+******************************************************************/
+
+const timer = setInterval(() => {
+	      if(Object.keys(orderItems).length === 3) {
+		const button = document.querySelector('.orderBtn');
+		button.children[0].innerHTML = "Fechar pedido";
+		button.children[1].innerHTML = "";
+		button.disabled = false;
+		button.classList.add('complete');
+		clearInterval(timer);
+	      }
+}, 20);
+
+
+
+
+
+
+/******************************************************************
+		THIS FUNCTION CONFIRMS THE ORDER
+******************************************************************/
+
+function checkout() {
+	const confirm_screen = document.querySelector('.order-confirmation-screen');
+	const app_container = document.querySelector('.app-container');
+	app_container.style.backdropFilter = "darkness(10)";
+	confirm_screen.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
